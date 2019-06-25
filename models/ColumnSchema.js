39fea68b9deaa687ab56834ca3e2f04c4afba8c1
Sujil Shah <sujil.shah@gmail.com>
2019-06-25 04:49:13 -0400

    Fixing the widget mapping

--- models/ColumnSchema.js
+++ models/ColumnSchema.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const ColumnSchema = mongoose.Schema({
     title: String,
     span: {type: Number, default: 2},
-    columns: [{
+    widgets: [{
         title: String,
-        type: {type: String, default: 'HEADING', enum: ['HEADING', 'YOUTUBE', 'HTML']},
+        type: {type: String, default: 'HEADING', enum: ['HEADING', 'PARAGRAPH', 'YOUTUBE', 'HTML']},
         text: {type: String, default: 'New Heading Widget'},
         size: {type: Number, default: 2}
     }]
